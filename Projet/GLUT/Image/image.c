c7c5c866df515c23f96163649ab1ac7d9cd3a88d
Cédric Evrard <patmol@Cedrics-MacBook-Pro.local>
2018-02-11 14:45:35 +0100

    Update image error

--- Projet/GLUT/Image/image.c
+++ Projet/GLUT/Image/image.c
@@ -74,7 +74,7 @@ void display() {
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
     glutCreateWindow("Image");
 
